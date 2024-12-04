ccf383e1405bb92b93736f26809e6b00de393f64
luijben <luijben@astron.nl>
2024-12-04 18:25:06 +0100

    Day 4

--- src/days/4/puzzle.ts
+++ src/days/4/puzzle.ts
@@ -1,16 +1,23 @@
 import Puzzle from "../../types/Puzzle";
+import { groupBy } from "../../utils/collection";
 import { splitOnNewLines } from "../../utils/input";
 
 export const puzzle = () : Puzzle => {
-    const find = (grid: string[][], xIndex: number, yIndex: number, xDirection: number, yDirection: number, target: string) => {
+    const linesToGrid = (lines: string[]) => lines.map(line => line.split(""));
+
+    const findWord = (grid: string[][], xIndex: number, yIndex: number, xDirection: number, yDirection: number, word: string) : Boolean => {
         let counter = 0;
 
-        while (counter < target.length) {
+        while (counter < word.length) {
             const nextX = xIndex + xDirection * counter;
             const nextY = yIndex + yDirection * counter;
+            
+            if (!grid[nextY]) {
+                return false;
+            }
 
-            const next = grid[nextY]![nextX];
-            if (next !== target.charAt(counter)) {
+            const next = grid[nextY][nextX];
+            if (next !== word.charAt(counter)) {
                 return false;
             }
             counter++;
@@ -19,18 +26,60 @@ export const puzzle = () : Puzzle => {
         return true;
     }
 
+    const inputToMatches = (input: string, directions: {x: number, y: number}[], targetWord: string) => {
+        const lines = splitOnNewLines(input);
+        const grid = linesToGrid(lines);
+        const regex = new RegExp(targetWord[0]!, 'g');
+        
+        const targetStartIndices = lines.flatMap((line, y) => [...line!.matchAll(regex)].map(match => ({ y, x: match.index })));
+
+        const matches = targetStartIndices.flatMap(
+            start => directions.filter(direction => findWord(grid, start.x, start.y, direction.x, direction.y, targetWord)).map(direction => ({
+                indexX: start.x,
+                indexY: start.y,
+                directionX: direction.x,
+                directionY: direction.y
+            })));
+
+        return matches;
+    }
+
     return {
         first: function (input: string): string | number {
-            const lines = splitOnNewLines(input);
-            const grid = lines.map(line => line.split(""));
-            const indices = [...lines[3]!.matchAll(/X/g)].map(match => match.index);
+            const targetWord = "XMAS";
+            const directions = [
+                {x: -1, y: +1},
+                {x: 0, y: +1},
+                {x: 1, y: +1},
+        
+                {x: -1, y: 0},
+                {x: 0, y: 0},
+                {x: 1, y: 0},
+        
+                {x: -1, y: -1},
+                {x: 0, y: -1},
+                {x: 1, y: -1},
+            ];
 
-            const output = indices.map(i => find(grid, i, 3, 1, 0, "XMAS"));
-            return input;
+            const matches = inputToMatches(input, directions, targetWord);
+            
+            return matches.length.toString();
         },
 
         second: function (input: string): string | number {
-            return input;   
+            const targetWord = "MAS";
+            const directions = [
+                {x: -1, y: +1},
+                {x: 1, y: +1},
+                {x: -1, y: -1},
+                {x: 1, y: -1},
+            ];
+
+            const matches = inputToMatches(input, directions, targetWord);
+            const centerCoords = matches.map(mas => `${mas.indexY + mas.directionY * 1},${mas.indexX + mas.directionX * 1}`);
+            const xmases = Object.values(groupBy(centerCoords, c => c)).filter(grp => grp.length === 2);
+            
+            return xmases.length.toString();
         }
     }
 };
--- src/utils/collection.ts
+++ src/utils/collection.ts
@@ -3,4 +3,10 @@ const zip = <T1, T2>(firstList: Array<T1>, secondList: Array<T2>) : Array<[T1, T
 
 const sum = (nums: number[]) : number => nums.length ? nums.reduce((a, b) => a + b) : 0;
 
-export { zip, sum};
+const groupBy = <T, K extends keyof any>(arr: T[], key: (i: T) => K) =>
+    arr.reduce((groups, item) => {
+      (groups[key(item)] ||= []).push(item);
+      return groups;
+    }, {} as Record<K, T[]>);
+
+export { zip, sum, groupBy };
