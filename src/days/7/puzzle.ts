fafc27f82f226293e0422bc05e0bd330f2bda7e8
Robbie Luijben <robbie@luijben.tech>
2024-12-07 14:03:29 +0100

    Day 7 part 2

--- src/days/7/puzzle.ts
+++ src/days/7/puzzle.ts
@@ -1,5 +1,5 @@
 import Puzzle from "../../types/Puzzle";
-import { cartesian, sum } from "../../utils/collection";
+import { cartesian, sum } from "../../utils/collection" ;
 import { splitOnNewLines } from "../../utils/input";
 
 type Equation = {
@@ -8,15 +8,14 @@ type Equation = {
     possibleEquations: string[]
 };
 
-
 export const puzzle = () : Puzzle => {
     const evaluateFromLeftToRight = (eq: string) : number => {
-        const operands = eq.split(/\+|\*/g);
-        const operators = [...eq.matchAll(/\+|\*/g)].map(match => match[0]);
+        const operands = eq.split(/\+|\*|\|\|/g);
+        const operators = [...eq.matchAll(/\+|\*|\|\|/g)].map(match => match[0]);
 
         let result = operands.shift();
         while (operands.length) {
-            const expression = `${result}${operators.shift()}${operands.shift()}`;
+            const expression = `${result}${operators.shift()?.replace("||", "")}${operands.shift()}`;
             result = eval(expression);
         }
 
@@ -24,32 +23,37 @@ export const puzzle = () : Puzzle => {
     };
 
     const possibleEquations = (numbers: number[], operators: string[]) => {
-        const pqs = [...cartesian(numbers.map((n) => operators.map(o => `${n}${o}`)))].map(eq => eq.join("").slice(0, -1));
+        const pqs = [...cartesian(numbers.map((n, i) => operators.map(o => `${n}${i === numbers.length -1 ? "" : o}`)))].map(eq => eq.join(""));
         return [...new Set(pqs)];
     }
 
+    const inputToEquations = (input: string, operators: string[]) => {
+        const equations: Equation[] = 
+        splitOnNewLines(input).map(line => [...line.matchAll(/\d+/g)].map(match => parseInt(match[0])))
+        .map(line => 
+        ({
+            test: line[0],
+            numbers: line.slice(1)
+        } as Equation))
+        .map(equation => ({
+            ...equation,
+            possibleEquations: possibleEquations(equation.numbers, operators)
+        } as Equation));
+
+        return equations;
+    };
+
     return {
         first: function (input: string): string | number {
-            
-            const equations: Equation[] = 
-                splitOnNewLines(input).map(line => [...line.matchAll(/\d+/g)].map(match => parseInt(match[0])))
-                .map(line => 
-                ({
-                    test: line[0],
-                    numbers: line.slice(1)
-                } as Equation))
-                .map(equation => ({
-                    ...equation,
-                    possibleEquations: possibleEquations(equation.numbers, ["+", "*"])
-                } as Equation));
-                
+            const equations: Equation[] = inputToEquations(input, ["+", "*"]);
             const possibilities = equations.filter(eq => eq.possibleEquations.some(pe => evaluateFromLeftToRight(pe) == eq.test))
- 
             return sum(possibilities.map(p => p.test!));
         },
 
         second: function (input: string): string | number {
-            return 0;
+            const equations: Equation[] = inputToEquations(input, ["+", "*", "||"]);
+            const possibilities = equations.filter(eq => eq.possibleEquations.some(pe => evaluateFromLeftToRight(pe) == eq.test));
+            return sum(possibilities.map(p => p.test!));
         }
     }
 };
