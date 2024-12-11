ca251157000b0f58ac75146c1c202b67a72340fd
luijben <luijben@astron.nl>
2024-12-11 15:39:58 +0100

    Day 11 part 2 WIP

--- src/days/11/puzzle.ts
+++ src/days/11/puzzle.ts
@@ -1,57 +1,43 @@
 import Puzzle from "../../types/Puzzle";
+import { sum } from "../../utils/collection";
 
 export const puzzle = () : Puzzle => {
+
+    const blink = (stone: number)  => {
+        if (stone === 0) {
+            return [1];
+        } else if (stone.toString().length % 2 === 0) {
+            const str = stone.toString();
+            
+            const s1 = parseInt(str.slice(0, str.length / 2))
+            const s2 = parseInt(str.slice(str.length / 2, str.length));
+
+            return [s1, s2];
+        }
+        else {
+            return [stone * 2024];
+        }
+    };
+
     return {
         first: function (input: string): string | number {
-            const stones = input.split(" ").map(n => parseInt(n));
-            
-            let i = 0;
-            const blinkCount = 25;
+            const stonesInput = input.split(" ").map(n => parseInt(n));
 
-            const cache = new Map<number, number>();
-            const cache2 = new Map<number, number[]>();
+            let sum = 0;
+    
+            for (const stone of stonesInput) {
+                let result = [stone];
 
-            while (i < blinkCount) {
-                console.log(i+1 + " " + stones.length);
-                let stoneCount = stones.length;
-                
-                for (let j = 0; j < stones.length; j++) {
-                    const stone = stones[j]!;
-
-                    if (stone === 0) {
-                        stones[j] = 1;
-                    } else if (stone.toString().length % 2 === 0) {
-                        const str = stone.toString();
-                        const s1 = parseInt(str.slice(0, str.length / 2))
-                        const s2 = parseInt(str.slice(str.length / 2, str.length));
-
-                        if (cache2.has(stone)) {
-                            const nums = cache2.get(stone)!;
-                            stones[j] = nums[0]!;
-                            stones.splice(j + 1, 0, nums[1]!);
-                        } else {
-                            stones[j] = s1;
-                            stones.splice(j + 1, 0, s2);
-                            cache2.set(stone, [s1, s2]);
-                        }
-
-                        stoneCount = stones.length;
-                        j++;
-                    } else {
-                        if (cache.has(stones[j]!)) {
-                            stones[j] = cache.get(stones[j]!)!;
-                        } else {
-                            const n = stones[j]! * 2024;
-                            cache.set(stones[j]!, n);
-                            stones[j] = n;
-                        }
-                    }
+                for (let i = 0; i < 25; i++) {
+                    result = result.flatMap(r => blink(r));
                 }
-
-                i++;
+                
+                sum += result.length;
             }
+
             
-            return stones.length;
+
+            return sum;
         },
         second: function (input: string): string | number {
             return 0
