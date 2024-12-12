7bc6109c3a4dfc981d5c042decf2215cde0dc6de
Robbie Luijben <robbie@luijben.tech>
2024-12-12 19:27:32 +0100

    day 11

--- src/days/11/puzzle.ts
+++ src/days/11/puzzle.ts
@@ -1,46 +1,41 @@
 import Puzzle from "../../types/Puzzle";
 import { sum } from "../../utils/collection";
+import { memoize } from "../../utils/function";
 
 export const puzzle = () : Puzzle => {
 
-    const blink = (stone: number)  => {
+    let memoizedBlink: (stone: number, iteration: number, max: number) => number;
+    
+    const blink = (stone: number, iteration: number, max: number) : number  => {
+        if (iteration === max) {
+            return 1;
+        }
+
         if (stone === 0) {
-            return [1];
+            return memoizedBlink(1, iteration + 1, max);
         } else if (stone.toString().length % 2 === 0) {
             const str = stone.toString();
             
             const s1 = parseInt(str.slice(0, str.length / 2))
             const s2 = parseInt(str.slice(str.length / 2, str.length));
 
-            return [s1, s2];
+            return memoizedBlink(s1, iteration + 1, max) + blink(s2, iteration + 1, max);
         }
         else {
-            return [stone * 2024];
+            return memoizedBlink(stone * 2024, iteration + 1, max);
         }
     };
 
+    memoizedBlink = memoize(blink) as (stone: number, iteration: number, max: number) => number;
+
+    const inputToStones = (input: string) => input.split(" ").map(stone => parseInt(stone));
+   
     return {
         first: function (input: string): string | number {
-            const stonesInput = input.split(" ").map(n => parseInt(n));
-
-            let sum = 0;
-    
-            for (const stone of stonesInput) {
-                let result = [stone];
-
-                for (let i = 0; i < 25; i++) {
-                    result = result.flatMap(r => blink(r));
-                }
-                
-                sum += result.length;
-            }
-
-            
-
-            return sum;
+            return sum(inputToStones(input).map(stone => memoizedBlink(stone, 0, 25) ));
         },
         second: function (input: string): string | number {
-            return 0
+            return sum(inputToStones(input).map(stone => memoizedBlink(stone, 0, 75) ));
         }
     }
 }
